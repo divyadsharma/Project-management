8ce7712f58dbfca3d19f0e74ed04d43a08af6577
divyadsharma <dvsharma129@gmail.com>
2018-08-08 17:35:19 +0530

    Adding proper classes to admin view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,31 @@
 module ApplicationHelper
   def errors_for(object)
     if object.errors.any?
-        content_tag(:div, class: "panel panel-danger") do
-            concat(content_tag(:div, class: "panel-heading") do
-                concat(content_tag(:h4, class: "panel-title") do
-                    concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
-                end)
-            end)
-            concat(content_tag(:div, class: "panel-body") do
-                concat(content_tag(:ul) do
-                    object.errors.full_messages.each do |msg|
-                        concat content_tag(:li, msg)
-                    end
-                end)
-            end)
-        end
+      content_tag(:div, class: "panel panel-danger") do
+        concat(content_tag(:div, class: "panel-heading") do
+          concat(content_tag(:h4, class: "panel-title") do
+            concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
+          end)
+        end)
+        concat(content_tag(:div, class: "panel-body") do
+          concat(content_tag(:ul) do
+            object.errors.full_messages.each do |msg|
+              concat content_tag(:li, msg)
+            end
+          end)
+        end)
+      end
     end
+  end
+
+  def nav_class(links_arr)
+    navigation_class = ''
+    links_arr.each do |link|
+      recognized = Rails.application.routes.recognize_path(link)
+      if recognized[:controller] == params[:controller] && recognized[:action] == params[:action]
+        navigation_class += 'active '
+      end
+    end
+    navigation_class
   end
 end
